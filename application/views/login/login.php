5095b9655867f78dc4b51182f48333cc9c18fa00
SgtBagus <procw57@gmail.com>
2019-07-11 19:46:09 +0700

    index remove

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -33,14 +33,13 @@ if($this->session->userdata('session_sop')!="") {
           </span>
 
           <span class="login100-form-title p-b-34 p-t-27">
-           <b>LOGIN GENERATOR</a>
+           <b>LOGIN GENERATOR
            </span>
            <div class="show_error"></div>
            <div class="wrap-input100 validate-input" data-validate = "Enter Email">
             <input class="input100" type="text" name="email" placeholder="Email">
             <span class="focus-input100" data-placeholder="&#xf207;"></span>
           </div>
-
           <div class="wrap-input100 validate-input" data-validate="Enter password">
             <input class="input100" type="password" name="password" placeholder="Password">
             <span class="focus-input100" data-placeholder="&#xf191;"></span>
