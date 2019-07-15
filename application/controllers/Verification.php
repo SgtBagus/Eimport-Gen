4bb5e3c19482f31eefbd2c5cfa711eefb12ca939
SgtBagus <procw57@gmail.com>
2019-07-15 11:55:52 +0700

    email send hosting

--- application/controllers/Verification.php
+++ application/controllers/Verification.php
@@ -7,59 +7,125 @@ class Verification extends MY_Controller {
 		parent::__construct();
 	}
 
-    function user($id) 
-    {
+  function user($id) 
+  {
+    $user = $this->mymodel->selectDataone('user', array('id'=>$id));
+    $konfig = $this->mymodel->selectDataone('konfig', array('SLUG'=>'COPYRIGHT'));
 
-        $user = $this->mymodel->selectDataone('user', array('id'=>$id));
-        $email_template = $this->mymodel->selectDataone('konfig', array('SLUG'=>'email-template'));
+    $body = '
+    <table class="two-left-inner" width="500" cellspacing="0" cellpadding="0" border="0" align="center">
+    <tbody>
+    <tr>
+    <td style="background:#FFF;" class="editable" valign="top" contenteditable="false" align="center">
+    <table width="295" cellspacing="0" cellpadding="0" border="0" align="center">
+    <tbody>
+    <tr>
+    <td style="font-size:55px; line-height:55px;" valign="top" height="55" align="center">&nbsp;</td>
+    </tr>
+    <tr>
+    <td style="font-size:40px; line-height:40px;" valign="top" height="40" align="center">&nbsp;</td>
+    </tr>
+    <tr>
+    <td style="font-family:sans-serif, Verdana; font-size:22px; color:#4b4b4c; line-height:30px; font-weight:normal;" mc:edit="tm4-03" valign="top" align="center">
+    <multiline>Hello! </multiline>
+    <br>
+    '.$user['name'].'
+    </td>
+    </tr>
+    <tr> 
+    <td valign="top" align="center">&nbsp;</td>
+    </tr>
+    <tr>
+    <td style="font-family:sans-serif, Verdana; font-size:48px; color:#4b4b4c; line-height:50px; font-weight:bold;" mc:edit="tm4-04" valign="top" align="center">
+    <multiline>Selamat Datang !</multiline>
+    </td>
+    </tr>
+    <tr>
+    <td valign="top" align="center">&nbsp;</td>
+    </tr>
+    <tr>
+    <td style="font-family:sans-serif, Verdana; font-size:13px; color:#71746f; line-height:22px; font-weight:normal;" mc:edit="tm4-05" valign="top" align="center">
+    <multiline>Mohon lakukan varifikasi email anda dengan mengklik button dibawah ini !</multiline>
+    </td>
+    </tr>
+    <tr>
+    <td valign="top" align="center">
+    <table width="180" cellspacing="0" cellpadding="0" border="0" align="center">
+    <tbody>
+    <tr>
+    <td valign="top" height="30" align="center">&nbsp;</td>
+    </tr>
+    <tr>
+    <td style="background:#f04386; -moz-border-radius: 40px; border-radius: 40px; font-family:Verdana, Arial; font-size:14px; font-weight:bold; text-transform:uppercase; color:#FFF;" data-bgcolor="theme-bg" mc:edit="tm4-06" valign="middle" height="60" align="center">
+    <multiline>
+    <a href="#" style="text-decoration:none; color:#FFF;">Konfirmasi Email</a>
+    </multiline>
+    </td>
+    </tr>
+    </tbody>
+    </table>
+    </td>
+    </tr>
+    <tr>
+    <td style="font-size:30px; line-height:30px;" valign="top" height="30" align="center">&nbsp;</td>
+    </tr>
+    </tbody>
+    </table>
+    </td>
+    </tr>
+    </tbody>
+    </table>
 
-        $to_email = $user['email'];
-        echo 'to_email : '; var_dump($to_email); echo'<br>';
-        $from_email =  'admin@karyastudio.com';
-        echo 'from_email : '; var_dump($from_email); echo'<br>';
-        $subject = 'Verification Email !';
-        echo 'subject : '; var_dump($subject); echo'<br>';
+    <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center">
+    <tbody>
+    <tr>
+    <td valign="top" align="center">
+    <table class="two-left-inner" width="500" cellspacing="0" cellpadding="0" border="0" align="center">
+    <tbody>
+    <tr>
+    <td valign="top" align="center">
+    <table width="260" cellspacing="0" cellpadding="0" border="0" align="center">
+    <tbody>
+    <tr>
+    <td style="font-size:40px; line-height:40px;" valign="top" height="40" align="center">&nbsp;</td>
+    </tr>
+    <tr>
+    <td style="font-family:sans-serif, Verdana; font-size:12px; color:#4b4b4c; line-height:30px; font-weight:normal;" mc:edit="tm4-11" valign="top" align="center">
+    <multiline>'.$konfig['value'].'</multiline>
+    </td>
+    </tr>
+    </tbody>
+    </table>
+    </td>
+    </tr>
+    </tbody>
+    </table>
+    </td>
+    </tr>
+    </tbody>
+    </table>';
 
-
-        $title = 'Coba : ';
-        // echo $title; 
-        // echo $email_template['value'];
-        // echo eval('return '. $email_template['value'] . ';');
-
-        // $body = $email_template['value'];
-
-        // echo 'body : '; var_dump($body); echo'<br>';
-
-
-
-        // mail($to_email, $subject, $body, $headers);
-
-
-        $config = Array(
-          'protocol' => 'smtp',
-          'smtp_host' => 'ssl://smtp.googlemail.com',
-          'smtp_port' => 465,
-          'smtp_user' => 'procw57@gmail.com', // change it to yours
-          'smtp_pass' => 'Andhikab57chan', // change it to yours
-          'mailtype' => 'html',
-          'charset' => 'iso-8859-1',
-          'wordwrap' => TRUE
-      );
-
-        $message = $email_template['value'];
-        $this->load->library('email', $config);
-        $this->email->set_newline("\r\n");
-        $this->email->from('localhost@gmail.com'); // change it to yours
-        $this->email->to('procw57@gmail.com');// change it to yours
-        $this->email->subject('Resume from JobsBuddy for your Job posting');
-        $this->email->message($message);
-        if($this->email->send())
-        {
-            echo 'Email sent.';
-        }
-        else
-        {
-            show_error($this->email->print_debugger());
-        }
-    }   
+    $config = Array(
+      'protocol' => 'smtp',
+           'smtp_host' => 'ssl://mail.karyastudio.com',
+           'smtp_port' => 465,
+           'smtp_user' => 'muhammadsafreza@karyastudio.com', // change it to yours
+           'smtp_pass' => 'loginloginlogin', // change it to yours
+           'mailtype' => 'html',
+           'charset' => 'iso-8859-1',
+           'wordwrap' => TRUE
+    );
+    
+    $this->load->library('email', $config);
+    $this->email->set_newline("\r\n");
+    $this->email->from('muhammadsafreza@karyastudio.com','TOR EROKOMENDASI'); // change it to yours
+    $this->email->to($user['email']);// change it to yours
+    $this->email->subject('Varifikasi Email Anda !');
+    $this->email->message('Ini Isinya COba');
+    if($this->email->send()){
+           echo "<script>alert('sukses dikirim');</script>";
+    }else{
+      show_error($this->email->print_debugger());
+    }
+  }
 }
---
+++ application/views/email/index.php
@@ -0,0 +1,91 @@
+<table class="two-left-inner" width="500" cellspacing="0" cellpadding="0" border="0" align="center">
+  <tbody>
+    <tr>
+      <td style="background:#FFF;" class="editable" valign="top" contenteditable="false" align="center">
+        <table width="295" cellspacing="0" cellpadding="0" border="0" align="center">
+          <tbody>
+            <tr>
+              <td style="font-size:55px; line-height:55px;" valign="top" height="55" align="center">&nbsp;</td>
+            </tr>
+            <tr>
+              <td style="font-size:40px; line-height:40px;" valign="top" height="40" align="center">&nbsp;</td>
+            </tr>
+            <tr>
+              <td style="font-family:'Open Sans', sans-serif, Verdana; font-size:22px; color:#4b4b4c; line-height:30px; font-weight:normal;" mc:edit="tm4-03" valign="top" align="center">
+                <multiline>Hello! </multiline>
+                <br>
+                <?= $name ?>
+              </td>
+            </tr>
+            <tr> 
+              <td valign="top" align="center">&nbsp;</td>
+            </tr>
+            <tr>
+              <td style="font-family:'Open Sans', sans-serif, Verdana; font-size:48px; color:#4b4b4c; line-height:50px; font-weight:bold;" mc:edit="tm4-04" valign="top" align="center">
+                <multiline><?= $title ?> </multiline>
+              </td>
+            </tr>
+            <tr>
+              <td valign="top" align="center">&nbsp;</td>
+            </tr>
+            <tr>
+              <td style="font-family:'Open Sans', sans-serif, Verdana; font-size:13px; color:#71746f; line-height:22px; font-weight:normal;" mc:edit="tm4-05" valign="top" align="center">
+                <multiline><?= $content ?></multiline>
+              </td>
+            </tr>
+            <tr>
+              <td valign="top" align="center">
+                <table width="180" cellspacing="0" cellpadding="0" border="0" align="center">
+                  <tbody>
+                    <tr>
+                      <td valign="top" height="30" align="center">&nbsp;</td>
+                    </tr>
+                    <tr>
+                      <td style="background:#f04386; -moz-border-radius: 40px; border-radius: 40px; font-family:'Open Sans', Verdana, Arial; font-size:14px; font-weight:bold; text-transform:uppercase; color:#FFF;" data-bgcolor="theme-bg" mc:edit="tm4-06" valign="middle" height="60" align="center">
+                        <multiline>
+                          <?= $button ?>
+                        </multiline>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+            <tr>
+              <td style="font-size:30px; line-height:30px;" valign="top" height="30" align="center">&nbsp;</td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+<table width="100%" cellspacing="0" cellpadding="0" border="0" align="center">
+  <tbody>
+    <tr>
+      <td valign="top" align="center">
+        <table class="two-left-inner" width="500" cellspacing="0" cellpadding="0" border="0" align="center">
+          <tbody>
+            <tr>
+              <td valign="top" align="center">
+                <table width="260" cellspacing="0" cellpadding="0" border="0" align="center">
+                  <tbody>
+                    <tr>
+                      <td style="font-size:40px; line-height:40px;" valign="top" height="40" align="center">&nbsp;</td>
+                    </tr>
+                    <tr>
+                      <td style="font-family:'Open Sans', sans-serif, Verdana; font-size:12px; color:#4b4b4c; line-height:30px; font-weight:normal;" mc:edit="tm4-11" valign="top" align="center">
+                        <multiline><?= $COPYRIGHT ?></multiline>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+  </tbody>
+</table>
